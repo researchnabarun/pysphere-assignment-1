29691afea92b3b00b06029d4f3934f67d0268b41
researchnabarun <researchnabarun@gmail.com>
2025-08-10 00:00:49 +0530

    Added solution for Ohm's Law Assignment

---
+++ ohms_law.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#print("hello worlds")
+
+current=float(input("enter current:"))
+resistance=float(input("enter resistance:"))
+voltage=current*resistance
+
+print("The voltage is",voltage)
